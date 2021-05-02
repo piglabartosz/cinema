aa6d9640aaeda53d8e0429e3662ebd0e960eff4b
Bartosz Pigla <piglabartosz@gmail.com>
2021-05-02 13:18:32 +0200

    Remove data loaders

--- src/main/kotlin/com/cinema/movie/OmdbService.kt
+++ src/main/kotlin/com/cinema/movie/OmdbService.kt
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.util.UriComponentsBuilder
 
 @Service
 class OmdbService(
